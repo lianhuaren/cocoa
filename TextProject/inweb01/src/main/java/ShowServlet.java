0fbed72483e3f24d06b1550166ac0fc2ab422c07
lbb <lbb@tempdemacbook-pro.local>
2018-11-25 18:42:57 +0800

    add intellij web01

---
+++ TextProject/inweb01/src/main/java/ShowServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
+
+    public void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+        out.print("Hello Gradle");
+        out.close();
+    }
+}
